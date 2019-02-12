3b3c5d247ab1f8f121cc2ee0e307fe5d7a4f69b6
brandon-m-navarro <brandon.m.navarro@gmail.com>
2019-02-11 23:40:43 -0500

    Back Button Bitches

--- src/main/java/UI/BackButtonController.java
+++ src/main/java/UI/BackButtonController.java
@@ -12,27 +12,30 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BackButtonController implements Initializable {
+
     @FXML
     JFXHamburger Hamburger;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition();
-        Hamburger.addEventHandler(MouseEvent.MOUSE_ENTERED_TARGET, new EventHandler<MouseEvent>() {
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(Hamburger);
+        Hamburger.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                transition.setRate(-1);
-                Hamburger.setAnimation(transition);
+                transition.setRate(transition.getRate()*-1);
+                transition.play();
+                System.out.println("Got Here!!");
             }
         });
-    Hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-        @Override
-        public void handle(MouseEvent event) {
-            AttributeContainer ac = AttributeContainer.getInstance();
-            ac.backlog.pop();
-            ac.current_page.goToPage((ac.backlog.pop()));
-        }
-    });
+        Hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                AttributeContainer ac = AttributeContainer.getInstance();
+                ac.backlog.pop();
+                ac.current_page.goToPage((ac.backlog.pop()));
+            }
+        });
     }
 
 }
