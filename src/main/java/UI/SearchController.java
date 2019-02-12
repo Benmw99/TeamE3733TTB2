62f4751e845b53bd9e5edb2643efdfdb7f6be1cb
Ben W <benmw10@gmail.com>
2019-02-12 17:26:04 -0500

    Updated fx:ids of HomeSearch

---
+++ src/main/java/UI/SearchController.java
@@ -0,0 +1,18 @@
+package UI;
+
+import Entities.AlcoholType;
+import Entities.Form;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.sql.Timestamp;
+
+public class SearchController {
+
+
+
+
+}
