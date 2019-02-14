e396d2289f0109a3759e1b23e58cf74de5d90ecd
jgold189 <climbjn189@gmail.com>
2019-02-14 16:50:07 -0500

    put stuff back in main for testing

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,21 +3,19 @@ import Entities.*;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Iterator;
 
 public class main {
     //private static SessionFactory factory;
     public static void main(String[] args) {
         DB.Database db = DB.Database.getDatabase();
-        //db.tableBuilder.resetDB();
-        //db.dbInsert.insertCompany(new Manufacturer(123, "Brewers Inc.", "manu", "manu"));
-        //db.dbInsert.insertAgent(new Agent("ttb", "ttb", "Sam Adamster"));
-        //db.dbInsert.insertRep(new Representative("123abc", "TestRep", "1234"));
+        db.tableBuilder.resetDB();
+        db.dbInsert.insertCompany(new Manufacturer(123, "Brewers Inc.", "manu", "manu"));
+        db.dbInsert.insertAgent(new Agent("ttb", "ttb", "Sam Adamster"));
+        db.dbInsert.insertRep(new Representative("123abc", "TestRep", "1234"));
         //db.dbInsert.insertData();
         System.out.println("Hello Iteration 2!");
 
         //Oh boy lets make a complete form
-        /*
         List<BrewersPermit> Brews = new ArrayList<>();
         Brews.add(new BrewersPermit("123ABC", true));
         Brews.add(new BrewersPermit("456DEF", false));
@@ -33,13 +31,12 @@ public class main {
                 "Bubbly", "BU", Adds, "John Smith", null, null,
                 "1112223333", "john@johnmail.com", "No other info", d, 123,
                 new Approval(), (float)12.3, ApprovalStatus.Incomplete);
-        form.setWorkingOn(ttb.getAgentID());
+        form.setWorkingOn(0);
 
 
         //AttributeContainer.getInstance().formQueue.add(form);
 
         db.dbInsert.insertForm(form);
-        */
         UI.MainUI.main( args);
     }
 
