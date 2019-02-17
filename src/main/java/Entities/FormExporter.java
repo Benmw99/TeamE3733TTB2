782886d2f9b9d605081646c7993327c30fb0dd3d
mjclements <mjclements@wpi.edu>
2019-02-17 18:16:38 -0500

    started work on form Exporter

--- src/main/java/Entities/FormExporter.java
+++ src/main/java/Entities/FormExporter.java
@@ -1,4 +1,33 @@
 package Entities;
+import org.apache.poi.POIDocument;
+import org.apache.poi.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class FormExporter {
+    public POIDocument document;
+
+    public FormExporter(){
+        this.document = new POIDocument() {
+            @Override
+            public void write() throws IOException {
+
+            }
+
+            @Override
+            public void write(File newFile) throws IOException {
+
+            }
+
+            @Override
+            public void write(OutputStream out) throws IOException {
+
+            }
+        }
+    }
+
+
+
 }
