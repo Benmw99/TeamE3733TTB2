99a9be8a502a0b10998fb9636dff0111071986c7
Parker <Pagrant@wpi.edu>
2019-02-17 19:49:43 -0500

    Started Finger Scanner more better more gooder

--- src/main/java/Entities/FingerScannerWindowsJNI.c
+++ src/main/java/Entities/FingerScannerWindowsJNI.c
@@ -1,7 +1,7 @@
 // Save as "HelloJNI.c"
 #include <jni.h>        // JNI header provided by JDK
 #include <stdio.h>      // C Standard IO Header
-#include "HelloJNI.h"   // Generated
+#include "FingerScannerWindowsJNI.h"   // Generated
 
 // Implementation of the native method sayHello()
 JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
