ba9f3d6ab75d59801d17ece371a87fd9f2a968d7
Parker <Pagrant@wpi.edu>
2019-02-17 19:56:01 -0500

    Started Finger Scanner more better more gooder wait actually bad not good dont use very bad

---
+++ src/main/java/Entities/compile.sh
@@ -0,0 +1,2 @@
+c:/Users/Parker/IdeaProjects/TeamE3733TTB2/src/main/java/Entities
+gcc -I"C:/Program Files/Java/jdk1.8.0_191/include" -I"C:/Program Files/Java/jdk1.8.0_191/include/win32" -o -shared FingerScannerWindowsJNI.c
