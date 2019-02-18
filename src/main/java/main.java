46b510e1ff8eaa7df3cf8d30104b67b37c5800fd
neusman1 <nick.eusman@gmail.com>
2019-02-18 14:41:13 -0500

    slight fxml changes to search

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -44,7 +44,7 @@ public class main {
         Form form = new Form(null, Brews, true, "00123SE", AlcoholType.MaltBeverage,
                 "Bubbly", "BU", Adds, "John Smith", null, null,
                 "1112223333", "mjclements@wpi.edu", "No other info", d, 123,
-                new Approval(), (float)12.3, ApprovalStatus.Incomplete);
+                new Approval(), (float)12.3, ApprovalStatus.Complete);
         form.setWorkingOn(0);
 
 
