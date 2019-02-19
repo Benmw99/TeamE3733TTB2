0bd8c747b199aed322c0237a6692b7ed2322723c
jgold189 <climbjn189@gmail.com>
2019-02-19 03:28:22 -0500

    Wine searching is back. Cleaned up dbselect a little bit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -19,39 +19,45 @@ public class main {
         MLogSystem.setLevel(Level.WARNING);
 
         DB.Database db = DB.Database.getDatabase();
-        db.tableBuilder.resetDB();
-        db.dbInsert.insertCompany(new Manufacturer(123, "Brewers Inc.", "manu", "manu"));
-        db.dbInsert.insertCompany(new Manufacturer(420, "Michael's Company", "mike", "1234"));
-        db.dbInsert.insertAgent(new Agent("ttb", "ttb", "Sam Adamster"));
-    //    db.dbInsert.insertAgent(new Agent("wwong2@wpi.edu", "pass", "Wilson Wong"));
-        db.dbInsert.insertAgent(new Agent("mjclements@wpi.edu", "1234", "Michael Clements"));
-        db.dbInsert.insertRep(new Representative("123abc", "TestRep", "1234"));
-        //db.dbInsert.insertData();
-
-
-        //Oh boy lets make a complete form
-        List<BrewersPermit> Brews = new ArrayList<>();
-        Brews.add(new BrewersPermit("123ABC", true));
-        Brews.add(new BrewersPermit("456DEF", false));
-
-        List<Address> Adds = new ArrayList<>();
-        Adds.add(new Address("Worcester", "MA", "01609", "100 Institue Road", "John Smith", true));
-        Adds.add(new Address("Acton", "MA", "123456", "2 Street Ave", "Bob Dijon", false));
-
-        long milli = System.currentTimeMillis();
-        Date d = new Date(milli);
-
-        Form form = new Form(null, Brews, true, "00123SE", AlcoholType.MaltBeverage,
-                "Bubbly", "BU", Adds, "John Smith", null, null,
-                "1112223333", "mjclements@wpi.edu", "No other info", d, 123,
-                new Approval(), (float)12.3, ApprovalStatus.Complete);
-        form.setWorkingOn(0);
-
-
-        //AttributeContainer.getInstance().formQueue.add(form);
-
-        db.dbInsert.insertForm(form);
-
+//        db.tableBuilder.resetDB();
+//        db.dbInsert.insertCompany(new Manufacturer(123, "Brewers Inc.", "manu", "manu"));
+//        db.dbInsert.insertCompany(new Manufacturer(420, "Michael's Company", "mike", "1234"));
+//        db.dbInsert.insertAgent(new Agent("ttb", "ttb", "Sam Adamster"));
+//    //    db.dbInsert.insertAgent(new Agent("wwong2@wpi.edu", "pass", "Wilson Wong"));
+//        db.dbInsert.insertAgent(new Agent("mjclements@wpi.edu", "1234", "Michael Clements"));
+//        db.dbInsert.insertRep(new Representative("123abc", "TestRep", "1234"));
+//        //db.dbInsert.insertData();
+//
+//
+//        /*//Oh boy lets make a complete form
+//        List<BrewersPermit> Brews = new ArrayList<>();
+//        Brews.add(new BrewersPermit("123ABC", true));
+//        Brews.add(new BrewersPermit("456DEF", false));
+//
+//        List<Address> Adds = new ArrayList<>();
+//        Adds.add(new Address("Worcester", "MA", "01609", "100 Institue Road", "John Smith", true));
+//        Adds.add(new Address("Acton", "MA", "123456", "2 Street Ave", "Bob Dijon", false));
+//
+//        long milli = System.currentTimeMillis();
+//        Date d = new Date(milli);
+//
+//        Form form = new Form(null, Brews, true, "00123SE", AlcoholType.MaltBeverage,
+//                "Bubbly", "BU", Adds, "John Smith", null, null,
+//                "1112223333", "mjclements@wpi.edu", "No other info", d, 123,
+//                new Approval(), (float)12.3, ApprovalStatus.Complete);
+//        form.setWorkingOn(0);
+//
+//
+//        //AttributeContainer.getInstance().formQueue.add(form);
+//
+//        db.dbInsert.insertForm(form);*/
+//
+//        System.out.println("*****************06 Data*************************");
+//        db.dbInsert.insertData06();
+//        System.out.println("*****************09 Data*************************");
+//        db.dbInsert.insertData09();
+//        System.out.println("*****************12 Data*************************");
+//        db.dbInsert.insertData12();
 
         System.out.println("Hello Iteration 2!");
         UI.MainUI.main( args);
