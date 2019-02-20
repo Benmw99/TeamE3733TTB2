2efae92919287b26b6f31c5ee74ddcc0382c85af
brandon-m-navarro <brandon.m.navarro@gmail.com>
2019-02-20 02:25:39 -0500

    Fixing UI for presentation

--- src/main/java/UI/ManSingleAppPageController.java
+++ src/main/java/UI/ManSingleAppPageController.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
-
+@Deprecated
 public class ManSingleAppPageController extends PageControllerUI implements  Initializable {
     @FXML
     public JFXButton SendApp;
