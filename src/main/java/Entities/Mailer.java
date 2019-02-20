8510d83bac4b101e5f2c01bcf606678388c00385
edelmonaco <edelmonaco@wpi.edu>
2019-02-19 20:45:24 -0500

    Added attach application to email

--- src/main/java/Entities/Mailer.java
+++ src/main/java/Entities/Mailer.java
@@ -5,8 +5,19 @@ import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+
+import javax.mail.*;
+import javax.mail.internet.MimeMultipart;
 
 
 public class Mailer implements Runnable {
@@ -19,10 +30,6 @@ public class Mailer implements Runnable {
     /**
      * @author Michael & Elizabeth
      */
-
-  //  public Mailer(){
-
-    //}
 
     /**
      * The constructor for a manufacturer inform thread Mailer
@@ -67,7 +74,7 @@ public class Mailer implements Runnable {
 
             InternetAddress toAddress = new InternetAddress();
 
-                toAddress = new InternetAddress(to_inform.getEmail());
+            toAddress = new InternetAddress(to_inform.getEmail());
             System.out.println(Message.RecipientType.TO);
 
             message.addRecipient(Message.RecipientType.TO, toAddress);
@@ -76,9 +83,8 @@ public class Mailer implements Runnable {
             body += ",\n";
             body += "There are updates on your form! Please use the TTB Application to check your status.\n";
             body += "Your form's current status is " + to_inform.getApprovalStatus().toString();
-            body += "\n Sincerely yours,";
-            body += "The Ebony Elves' TTB Application";
-            message.setText(body);
+            mailHelper(message, body);
+
             message.setSubject("RE: TTB APP UPDATE");
             Transport transport = session.getTransport("smtp");
             transport.connect(host, from, pass);
@@ -89,6 +95,26 @@ public class Mailer implements Runnable {
             mx.printStackTrace();
         }
 
+    }
+
+    private void mailHelper(MimeMessage message, String body) throws MessagingException {
+        body += "\n Sincerely yours,";
+        body += "The Ebony Elves' TTB Application";
+
+        Multipart multi = new MimeMultipart();
+
+        MimeBodyPart textBodyPart = new MimeBodyPart();
+        textBodyPart.setText(body);
+
+        MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+        DataSource src = new FileDataSource("output.docx");
+        attachmentBodyPart.setDataHandler(new DataHandler(src));
+        attachmentBodyPart.setFileName("TTB Application Form.docx");
+
+        multi.addBodyPart(textBodyPart);
+        multi.addBodyPart(attachmentBodyPart);
+
+        message.setContent(multi);
     }
 
     /**
@@ -125,9 +151,8 @@ public class Mailer implements Runnable {
             body += "You have been assigned a new form. Please log in and check your TTB Form Queue.\n";
             body += "The sending agent has specified the following message: \n";
             body += msg + "\n";
-            body += "\n Sincerely yours,";
-            body += "The Ebony Elves' TTB Application";
-            message.setText(body);
+            mailHelper(message, body);
+
             message.setSubject("RE: TTB QUEUE UPDATE");
             Transport transport = session.getTransport("smtp");
             transport.connect(host, from, pass);
