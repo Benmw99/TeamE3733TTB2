2661158c64fee9cf9c442e0997418bf3862ce619
mjclements <mjclements@wpi.edu>
2019-02-26 23:30:37 -0500

    everything works............... i do not know by what power i have wrought this
    this is terrifying.

--- src/main/java/Entities/compile.sh
+++
@@ -1,2 +0,0 @@
-c:/Users/Parker/IdeaProjects/TeamE3733TTB2/src/main/java/Entities
-gcc -I"C:/Program Files/Java/jdk1.8.0_191/include" -I"C:/Program Files/Java/jdk1.8.0_191/include/win32" -o -shared FingerScannerWindowsJNI.c
