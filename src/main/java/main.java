d9787bb0e4a7388f3f281cae60a5736317b2e402
jgold189 <climbjn189@gmail.com>
2019-02-27 00:56:44 -0500

    Slight change to main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,42 +1,32 @@
 import Entities.*;
 import SearchAlgo.EnvSetter;
-import org.omg.SendingContext.RunTime;
-
-import java.lang.reflect.Field;
 import java.sql.Date;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.concurrent.*;
 
 public class main {
-
-
-    //private static SessionFactory factory;
     public static void main(String[] args) {
         EnvSetter env = new EnvSetter();
-            Map<String,String> map = new HashMap<String,String>();
-            map.put("GOOGLE_APPLICATION_CREDENTIALS", env.getPath("My First Project-b6981c3f2253.json"));
-            try {
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("GOOGLE_APPLICATION_CREDENTIALS", env.getPath("My First Project-b6981c3f2253.json"));
+        try {
+            env.setEnv(map);
+        } catch (Exception e){ }
 
-                env.setEnv(map);
-            } catch (Exception e){
+        System.out.println("Starting program...");
+        //FINALLY DISABLES THE INFO MESSAGES
+        Logger hibSystem = Logger.getLogger("org.hibernate");
+        hibSystem.setLevel(Level.WARNING);
+        Logger c3System = Logger.getLogger("com.mchange.v2.c3p0");
+        c3System.setLevel(Level.WARNING);
+        Logger MLogSystem = Logger.getLogger("com.mchange");
+        MLogSystem.setLevel(Level.WARNING);
+        //Mongodb info message disable doesn't work, I'm still searching for what it is called
+        Logger mongoSystem = Logger.getLogger("mongodb");
+        mongoSystem.setLevel(Level.WARNING);
 
-        }
-
-            System.out.println("Starting program...");
-            //FINALLY DISABLES THE INFO MESSAGES
-            Logger hibSystem = Logger.getLogger("org.hibernate");
-            hibSystem.setLevel(Level.WARNING);
-            Logger c3System = Logger.getLogger("com.mchange.v2.c3p0");
-            c3System.setLevel(Level.WARNING);
-            Logger MLogSystem = Logger.getLogger("com.mchange");
-            MLogSystem.setLevel(Level.WARNING);
-            //Mongodb info message disable doesn't work, I'm still searching for what it is called
-            Logger mongoSystem = Logger.getLogger("mongodb");
-            mongoSystem.setLevel(Level.WARNING);
-
-            DB.Database db = DB.Database.getDatabase();
+        DB.Database db = DB.Database.getDatabase();
 
         /*db.tableBuilder.resetDB();
 
@@ -74,29 +64,14 @@ public class main {
         db.dbInsert.insertForm(form);
 */
 
-            //System.out.println("*****************Data*************************");
-            //db.dbInsert.insertData();
+        //System.out.println("*****************Data*************************");
+        //db.dbInsert.insertData();
 
-            //System.out.println("Inserting Mongo info");
-            //db.mongoFunc.insertDataMongo();
-            List<BrewersPermit> Brews = new ArrayList<>();
-            Brews.add(new BrewersPermit("123ABC", true));
-            Brews.add(new BrewersPermit("456DEF", false));
+        //System.out.println("Inserting Mongo info");
+        //db.mongoFunc.insertDataMongo();
 
-            List<Address> Adds = new ArrayList<>();
-            Adds.add(new Address("Worcester", "MA", "01609", "100 Institute Road", "John Smith", true));
-            Adds.add(new Address("Acton", "MA", "123456", "2 Street Ave", "Bob Dijon", false));
+        System.out.println("Hello Iteration 2!");
+        UI.MainUI.main(args);
 
-            long milli = System.currentTimeMillis();
-            Date d = new Date(milli);
-
-            Form form = new Form(null, Brews, true, "00123SE", AlcoholType.MaltBeverage,
-                    "Bubbly", "BU", Adds, "John Smith", null, new WineFormItems(),
-                    "1112223333", "mjclements@wpi.edu", "No other info", d, 123,
-                    new Approval(), (float) 12.3, ApprovalStatus.Incomplete);
-            //       db.dbInsert.insertForm(form);
-            System.out.println("Hello Iteration 2!");
-            UI.MainUI.main(args);
-
-        }
+    }
 }
